3376c9755eeb8e9b7bf8e3e9fa441e1f957f712a
Thori Bonner <95255552+thoribonner@users.noreply.github.com>
2022-03-08 12:57:53 -0500

    add pretty print

--- src/config/logger.js
+++ src/config/logger.js
@@ -3,9 +3,13 @@ const { nanoid } = require("nanoid");
 
 const level = process.env.LOG_LEVEL || "info";
 
+const nodeEnv = process.env.NODE_ENV || "development";
+const prettyPrint = nodeEnv === "development";
+
 const logger = pinoHttp({
   genReqId: (request) => request.headers["x-request-id"] || nanoid(),
   level,
+  prettyPrint,
 });
 
 module.exports = logger;
